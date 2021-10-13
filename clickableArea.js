a16ddf7d9f2243ae4a6dc992d4384750970ad806
namnameeroo <69442847+namnameeroo@users.noreply.github.com>
2021-10-13 23:30:58 +0900

    Update clickableArea.js

--- clickableArea.js
+++ clickableArea.js
@@ -1,11 +1,11 @@
-		// clickable 만들기 수정중 
-//		$('#clickable-row').on('click', function(){
-//			console.log(sele)
-//			let checkbox = $(this).find('.custom-control-label');
-//			if (checkbox.selected) {
-//				console.log("selected")
-//			} else {
-//				console.log("unchecked")
-//			}
-//		    checkbox.prop("checked", !checkbox.prop("checked"));
-//		});
+clickable 만들기 수정중 
+$('#clickable-row').on('click', function(){
+	console.log(sele)
+	let checkbox = $(this).find('.custom-control-label');
+	if (checkbox.selected) {
+		console.log("selected")
+	} else {
+		console.log("unchecked")
+	}
+    checkbox.prop("checked", !checkbox.prop("checked"));
+});
