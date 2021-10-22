5d6c7191ca1ee101c5bb32836d0663cc9c9fb8cd
namnameeroo <69442847+namnameeroo@users.noreply.github.com>
2021-10-23 01:19:56 +0900

    Update fileDataValidation.js

--- fileDataValidation.js
+++ fileDataValidation.js
@@ -1,3 +1,16 @@
 // 1. html의 input type=file 속성값 설정으로 파일 제한
-// 2. js 파일의 
+let newInputLine = document.creatElement('input');
+newInputLine.id = 'file-input'
+newInputLine.type = 'file'
+newInputLine.setAttribute('accept','image/*, .jpg, .gif')
+
+
+
+let form = document.querySelector('#main-form')
+form.appendChild(newInputLine)
+
+
+
+// 🇰🇷🤪🤪🤪
+// 2. js 파일의 스
 
