572581a246f5e97a53f14604186a581080781484
namnameeroo <69442847+namnameeroo@users.noreply.github.com>
2021-10-27 00:12:14 +0900

    Update HigherOrderFunction.js

--- HigherOrderFunction.js
+++ HigherOrderFunction.js
@@ -1,12 +1,21 @@
-// 문제
+// 연습 1
 const nam = kim(5);   // nam = (b) => (c) => {return 5+b-c}
   const han = nam(2); // han = (c) => {return 5+2-c}
   const su = han(3);  // su  = return 5+2-3
   console.log(su);    // 따라서 = 4
 
-// 문제
+// 연습 2
 const func = (msg) => () => {
       console.log(msg);
     };
 const innerFunc1 = func("hello");
 innerFunc1();
+
+// 연습 3
+const minsu = (j) => love(j)
+
+function love(i) {
+    let sentence = "love " + i 
+    return sentence
+}
+minsu(hansu);
