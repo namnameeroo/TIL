745f59de1ce47eef2d3e105fb8937b2303f45629
namnameeroo <69442847+namnameeroo@users.noreply.github.com>
2021-12-14 17:26:16 +0900

    Update voteCount.sql
    최초의 poll_vote 조회시 join question, valid check 조건 추가

--- voteCount.sql
+++ voteCount.sql
@@ -1,10 +1,10 @@
 SELECT P.poll_id, P.title as title, COUNT(SV.poll_id) AS vote_count, P.create_at::date, 
 			P.end_at::date, P.host_id, (P.end_at::date - NOW()::date) as d_day , size, connect
 		FROM poll P 
-		LEFT JOIN (SELECT distinct(PV.poll_id), SUB.create_at 
-				FROM (SELECT distinct(create_at) FROM poll_vote) SUB
+		LEFT JOIN (SELECT distinct(PV.poll_id), SUB.create_at
+				FROM (SELECT distinct(subPV.create_at) FROM poll_vote subPV JOIN poll_question PQ on PQ.question_id = subPV.question_id WHERE PQ.valid = 1) SUB
 				JOIN poll_vote PV ON SUB.create_at = PV.create_at) SV ON P.poll_id = SV.poll_id
 		INNER JOIN public.user u ON u.member_id = P.host_id
-		WHERE P.valid=1 AND u.member_id = #{member_id}::integer
+		WHERE P.valid=1 AND u.member_id = 2::integer
 		GROUP BY P.poll_id
 		ORDER BY P.poll_id
